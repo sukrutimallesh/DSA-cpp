22b98a2fb19fc745f84e88baee37875831950ade
Sukruti Mallesh <sukrutimallesh@gmail.com>
2023-06-17 04:55:29 -0700

    Update promotedHouseCityStack.cpp

--- promotedHouseCityStack/promotedHouseCityStack.cpp
+++ promotedHouseCityStack/promotedHouseCityStack.cpp
@@ -1,14 +1,3 @@
-/** Single Programmer Affidavit
-
-I the undersigned promise that the submitted assignment is my own work. While I was
-free to discuss ideas with others, the work contained is my own. I recognize that
-should this not be the case; I will be subject to penalties as outlined in the course
-syllabus.
-
-Name: Sukruti Mallesh
-Red ID: 828540265
-*/
-
 #include "promotedHouseCityStack.h"
 #include <stdexcept> // header for logic_error exception class
 
